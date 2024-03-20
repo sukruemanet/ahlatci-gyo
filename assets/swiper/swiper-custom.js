b0c043b006db825c4a093ccd7d4b166f80ce74e2
SukruEmanet <sukruemanet@gmail.com>
2024-03-20 23:54:49 +0300

    test

--- assets/swiper/swiper-custom.js
+++ assets/swiper/swiper-custom.js
@@ -1,43 +1,7 @@
-﻿//Services Slider
-var swiper = new Swiper(".product-slider", {
-  slidesPerView: 'auto',
-  speed: 1000,
-  spaceBetween: 40,
-  grabCursor: true,
-  watchSlidesProgress: true,
-  watchSlidesVisibility: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1.2,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    768: {
-      slidesPerView: 3.2,
-      spaceBetween: 30
-    },
-    // when window width is >= 640px
-    1024: {
-      slidesPerView: 3.2,
-      spaceBetween: 40
-    },
-    2560: {
-      slidesPerView: 4.2,
-      spaceBetween: 40
-    }
-  },
-});
-
-//Blog Slider
-var swiper2 = new Swiper(".blogSlider", {
+﻿
+//Index Slider
+var swiper = new Swiper(".startSlider", {
   slidesPerView: 1,
-  draggable: true,
-  speed: 1000,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
