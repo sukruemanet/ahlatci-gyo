e72ac544c5adfc1f1c92a537bf9cfd351c31c84e
SukruEmanet <sukruemanet@gmail.com>
2024-03-24 13:28:59 +0300

    test

--- assets/swiper/swiper-custom.js
+++ assets/swiper/swiper-custom.js
@@ -2,6 +2,8 @@
 //Index Slider
 var swiper = new Swiper(".startSlider", {
   slidesPerView: 1,
+  parallax: true,
+  speed: 1500,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
