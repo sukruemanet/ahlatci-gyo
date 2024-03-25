c1d10c0ec69347867875e64fbd7af43defc1e328
SukruEmanet <sukruemanet@gmail.com>
2024-03-25 21:58:31 +0300

    test

--- assets/js/common/menu.js
+++ assets/js/common/menu.js
@@ -10,11 +10,38 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+  $(function () {
+      var header = $("header");
 
-    
+      $(window).scroll(function () {
+          var scroll = $(window).scrollTop();
+          if (scroll >= 50) {
+              header.addClass("scrolled");
+          } else {
+              header.removeClass("scrolled");
+          }
+      });
+  });
+});
 
+$(document).ready(function () {
+  'use strict';
+  var c, currentScrollTop = 0,
+      navbar = $('header');
 
+  $(window).scroll(function () {
+      var a = $(window).scrollTop();
+      var b = navbar.height();
 
+      currentScrollTop = a;
 
-
+      if (c < currentScrollTop && a > b + b) {
+          navbar.addClass("scrollUp");
+      } else if (c > currentScrollTop && !(a <= b)) {
+          navbar.removeClass("scrollUp");
+      }
+      c = currentScrollTop;
+  });
+})
 
